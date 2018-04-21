971ccbcec9f9f540e4900f9dfd01ffecc2e26b1f
Waraphon Roonnapai <eepw0211@gmail.com>
2018-04-21 18:03:32 +0700

    update 2

--- color.js
+++ color.js
@@ -8,23 +8,43 @@ var color = [
 ]
 
 var box = document.querySelectorAll(".box");
-var pickcolor = color[1];
+var pickcolor = color[pickcolors()];
+var i = 0;
+
+// Start Head text color
 var discolor = document.querySelector("#discolor");
 discolor.textContent = pickcolor;
 
-var i = 0
+
+// Start
 box.forEach(function(box) {
     box.style.background = color[i];
     i++
-
     box.addEventListener("click", function() {
         var clickcolor = box.style.background;
+        var wrong = document.querySelector("#aws");
 
-        if (clickcolor === pickcolor) {
-            alert("Right")
-        }
-        else {
-            alert("Wrong")
-        }
+	        if (clickcolor === pickcolor) {
+	            wrong.textContent = "That right !!";
+	            loopcolor(clickcolor);
+
+	        }
+	        else {
+	            wrong.textContent = "Wrong Play agin?";
+	            this.setAttribute("style","background: rgb(38, 38, 38);");
+
+	        }
     });
 });
+
+// Function chance Color to all box
+function loopcolor(corectcolor){
+	box.forEach( function(box) {
+		box.style.background = corectcolor ;
+	});
+}
+
+// Function Random Start pickcolor
+function pickcolors(){
+	return Math.floor(Math.random()*color.length);
+};
