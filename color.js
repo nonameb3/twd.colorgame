e04f62fc0b94bda0631641217e32b6d6e91c90f9
Waraphon Roonnapai <eepw0211@gmail.com>
2018-05-04 21:49:33 +0700

    delete some code

--- color.js
+++ color.js
@@ -85,27 +85,6 @@ function reset() {
     wrong.textContent = "";
 };
 
-
-// Start Main code for loop color
-// box.forEach(function(box) {
-//     box.style.background = color[i];
-//     i++
-//     box.addEventListener("click", function() {
-//         var clickcolor = box.style.background;
-//         var wrong = document.querySelector("#aws");
-//         if (clickcolor == pickcolor) {
-//             wrong.textContent = "That right !!";
-//             newgame.textContent = "Play again?";
-//             loopcolor(clickcolor);
-//             head.style.background = clickcolor;
-
-//         } else {
-//             wrong.textContent = "Wrong,Play agin?";
-//             this.setAttribute("style", "background: rgb(38, 38, 38);");
-//         };
-//     });
-// });
-
 // Function chance Color to all box
 function loopcolor(corectcolor) {
     box.forEach(function(box) {
